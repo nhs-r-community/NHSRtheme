19b73d04cc297e7b7e87ba769c8d371b605ac454
Tom Jemmett <tom.jemmett@gmail.com>
2019-03-09 21:18:18 +0000

    splits colours.R functions into seperate files

--- R/getNhsColours.R
+++ R/getNhsColours.R
@@ -3,32 +3,31 @@
 #' This function gets the colours as defined by the
 #' [NHS Identity](https://www.england.nhs.uk/nhsidentity/identity-guidelines/colours/)
 #'
-#' @param section a character, or list of characters of the colour sections to
-#'                 use
+#' @param ... optional names of the colours to return
+#' @param section optional, name of the colour sections to use
 #'
 #' @return named vector of hexadecimal strings of colours
-#' @export
-#'
 #' @importFrom dplyr %>%
 #' @importFrom purrr map_lgl
+#' @export
 #'
 #' @examples
 #' getNhsColours()
 #' getNhsColours("blues")
 #' getNhsColours(c("blues", "neutrals"))
 getNhsColours <- function (..., section = NULL) {
-  if (is.null(section)) {
-    section <- c("blues",
-                 "neutrals",
-                 "support greens",
-                 "highlights")
-  }
-
   dots <- unique(c(...))
   colours <- c()
 
+  if (!is.null(section)) {
+    section <- match.arg(section, c("blues",
+                                    "neutrals",
+                                    "support greens",
+                                    "highlights"))
+  }
+
   # Level 1: NHS blues
-  if ("blues" %in% section) {
+  if (is.null(section) || section == "blues") {
     colours <- c(colours,
       DarkBlue   = "#003087",
       Blue       = "#005EB8",
@@ -38,7 +37,7 @@ getNhsColours <- function (..., section = NULL) {
   }
 
   # Level 2: NHS neutrals
-  if ("neutrals" %in% section) {
+  if (is.null(section) || section == "neutrals") {
     colours <- c(colours,
       Black      = "#231f20",
       DarkGrey   = "#425563",
@@ -47,7 +46,7 @@ getNhsColours <- function (..., section = NULL) {
   }
 
   # Level 3: NHS support greens
-  if ("support greens" %in% section) {
+  if (is.null(section) || section == "support greens") {
     colours <- c(colours,
       Green      = "#009639",
       DarkGreen  = "#006747",
@@ -56,7 +55,7 @@ getNhsColours <- function (..., section = NULL) {
   }
 
   # Level 4: NHS highlights
-  if ("highlights" %in% section) {
+  if (is.null(section) || section == "highlights") {
     colours <- c(colours,
       Purple     = "#330072",
       DarkPink   = "#7C2855",
@@ -75,44 +74,6 @@ getNhsColours <- function (..., section = NULL) {
       stop("Invalid colours specified")
     }
   }
-
-  return (colours)
-}
-
-#' Get's tints of the NHS identity colours
-#'
-#' This function returns tints of the NHS identity colours
-#'
-#' @param tints a vector of numbers between 0 and 1 for the percentage colour
-#'               tint to use
-#' @param section a character, or list of characters of the colour sections to
-#'                 use
-#'
-#' @return named vector of hexadecimal strings of colours
-#' @importFrom dplyr %>%
-#' @importFrom purrr map_chr map set_names
-#' @importFrom scales percent
-#' @importFrom grDevices col2rgb
-#' @export
-#'
-#' @examples
-#' getNhsColourTints(seq(0.0,0.8,0.2))
-#' getNhsColourTints(seq(0.0,0.8,0.2), "blues")
-getNhsColourTints <- function (tints, ..., section = NULL) {
-  colours <- getNhsColours(..., section = section)
-
-  getTints <- function(colour) {
-    colTint <- function(tint) {
-      x <- as.vector(col2rgb(colour))
-      x <- sum(round(x + (255-x)*tint) * 16^c(4, 2, 0))
-      sprintf("#%06X", x)
-    }
-
-    map_chr(tints, colTint) %>%
-      set_names(percent(tints, 1))
-  }
-
-  colours <- map(colours, getTints) %>% unlist
 
   return (colours)
 }
