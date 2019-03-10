c01d347f580239673daec1f48e4787ffa87dec45
Tom Jemmett <tom.jemmett@gmail.com>
2019-03-10 19:33:00 +0000

    switches test to use a saved dataset

--- tests/testthat/test-getNhsColours.R
+++ tests/testthat/test-getNhsColours.R
@@ -1,5 +1,4 @@
 context("getNhsColours")
-library(nhsrtheme)
 library(dplyr)
 library(purrr)
 
@@ -10,74 +9,26 @@ test_that("getNhsColours returns a character vector", {
 })
 
 test_that("getNhsColours returns correct colours", {
-  expected <- c(DarkBlue   = "#003087",
-                Blue       = "#005EB8",
-                BrightBlue = "#0072CE",
-                LightBlue  = "#41B6E6",
-                AquaBlue   = "#00A9CE",
-                Black      = "#231f20",
-                DarkGrey   = "#425563",
-                MidGrey    = "#768692",
-                PaleGrey   = "#E8EDEE",
-                Green      = "#009639",
-                DarkGreen  = "#006747",
-                LightGreen = "#78BE20",
-                AquaGreen  = "#00A499",
-                Purple     = "#330072",
-                DarkPink   = "#7C2855",
-                Pink       = "#AE2573",
-                DarkRed    = "#8A1538",
-                Red        = "#DA291C",
-                Orange     = "#ED8B00",
-                WarmYellow = "#FFB81C",
-                Yellow     = "#FAE100")
-
+  expected <- readRDS("getNhsColours.rds")
   actual <- getNhsColours()
-
   expect_equal(actual, expected)
 })
 
 test_that("getNhsColours returns only the colour passed to it", {
-  expected <- c(DarkBlue   = "#003087",
-                Blue       = "#005EB8",
-                BrightBlue = "#0072CE",
-                LightBlue  = "#41B6E6",
-                AquaBlue   = "#00A9CE",
-                Black      = "#231f20",
-                DarkGrey   = "#425563",
-                MidGrey    = "#768692",
-                PaleGrey   = "#E8EDEE",
-                Green      = "#009639",
-                DarkGreen  = "#006747",
-                LightGreen = "#78BE20",
-                AquaGreen  = "#00A499",
-                Purple     = "#330072",
-                DarkPink   = "#7C2855",
-                Pink       = "#AE2573",
-                DarkRed    = "#8A1538",
-                Red        = "#DA291C",
-                Orange     = "#ED8B00",
-                WarmYellow = "#FFB81C",
-                Yellow     = "#FAE100")
-
+  expected <- readRDS("getNhsColours.rds")
   actual <- expected %>% names %>% map_chr(getNhsColours)
-
   expect_equal(actual, unname(expected))
 })
 
 test_that("getNhsColours returns all of the colours passed to it", {
-  expected <- c(DarkBlue   = "#003087",
-                AquaBlue   = "#00A9CE",
-                PaleGrey   = "#E8EDEE")
+  expected <- readRDS("getNhsColours.rds")[c("DarkBlue", "AquaBlue", "PaleGrey")]
   actual <- getNhsColours("DarkBlue", "AquaBlue", "PaleGrey")
-
   expect_equal(actual, expected)
 })
 
 test_that("getNhsColours ignores duplicated colour names", {
   expected <- 1
   actual <- getNhsColours("DarkBlue", "DarkBlue")
-
   expect_length(actual, expected)
 })
 
@@ -86,50 +37,21 @@ test_that("getNhsColours throws an error if an unrecognised colour name is passe
 })
 
 test_that("getNhsColours returns specific colours if a section is passed", {
-  # blues
-  expected <- c(DarkBlue   = "#003087",
-                Blue       = "#005EB8",
-                BrightBlue = "#0072CE",
-                LightBlue  = "#41B6E6",
-                AquaBlue   = "#00A9CE")
+  all_colours <- readRDS("getNhsColours.rds")
 
-  actual <- getNhsColours(section = "blues")
+  sections = list(
+    `blues` = c("DarkBlue", "Blue", "BrightBlue", "LightBlue", "AquaBlue"),
+    `neutrals` = c("Black", "DarkGrey", "MidGrey", "PaleGrey"),
+    `support greens` = c("Green", "DarkGreen", "LightGreen", "AquaGreen"),
+    `highlights` = c("Purple", "DarkPink", "Pink", "DarkRed", "Red", "Orange",
+                     "WarmYellow", "Yellow")
+  )
 
-  expect_equal(actual, expected)
-
-  # neutrals
-  expected <- c(Black      = "#231f20",
-                DarkGrey   = "#425563",
-                MidGrey    = "#768692",
-                PaleGrey   = "#E8EDEE")
-
-  actual <- getNhsColours(section = "neutrals")
-
-  expect_equal(actual, expected)
-
-  # support greens
-  expected <- c(Green      = "#009639",
-                DarkGreen  = "#006747",
-                LightGreen = "#78BE20",
-                AquaGreen  = "#00A499")
-
-  actual <- getNhsColours(section = "support greens")
-
-  expect_equal(actual, expected)
-
-  # highlights
-  expected <- c(Purple     = "#330072",
-                DarkPink   = "#7C2855",
-                Pink       = "#AE2573",
-                DarkRed    = "#8A1538",
-                Red        = "#DA291C",
-                Orange     = "#ED8B00",
-                WarmYellow = "#FFB81C",
-                Yellow     = "#FAE100")
-
-  actual <- getNhsColours(section = "highlights")
-
-  expect_equal(actual, expected)
+  map(names(sections), function(s) {
+    expected <- all_colours[sections[[s]]]
+    actual <- getNhsColours(section = s)
+    expect_equal(actual, expected)
+  })
 })
 
 test_that("getNhsColours throws an error if an incorrect section is passed", {
