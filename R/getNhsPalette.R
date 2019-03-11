3e9cb34f880c12b26b4ecc5742db365af8e85384
Thomas Jemmett <thomas.jemmett@nhs.net>
2019-03-11 08:35:02 +0000

    renames nhstheme_pal to getNhsPalette

--- R/getNhsPalette.R
+++ R/getNhsPalette.R
@@ -1,11 +1,11 @@
-#' Return function to interpolate a drsimonj color palette
+#' Return function to interpolate an Nhs theme colour palette
 #'
-#' @param palette Character name of palette in drsimonj_palettes
+#' @param palette Character name of section in Nhs theme
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments to pass to colorRampPalette()
 #'
 #' @importFrom grDevices colorRampPalette
-nhstheme_pal <- function(palette = "blues", reverse = FALSE, ...) {
+getNhsPalette <- function(palette = "blues", reverse = FALSE, ...) {
   pal <- getNhsColours(section = palette)
 
   if (reverse) pal <- rev(pal)
