af27702b3dd4235967ce293001e7c0b1c9e738bc
Thomas Jemmett <thomas.jemmett@nhs.net>
2019-03-11 08:49:28 +0000

    adds showNhsThemeColourTints function

---
+++ R/showNhsThemeColourTints.R
@@ -0,0 +1,37 @@
+#' Show's the NHS identity colours
+#'
+#' This function shows the tints of a colour from the Nhs Theme
+#'
+#'
+#' @return named vector of hexadecimal strings of colours
+#'
+#' @param tints a vector of numbers in [0, 1] that represents the percentage tints
+#'              required
+#' @param colour the name of the colour to show tints of
+#'
+#' @importFrom dplyr %>% group_by mutate tibble row_number
+#' @importFrom purrr map_dfr
+#' @import ggplot2
+#' @export
+#'
+#' @examples
+#' showNhsThemeColourTints(seq(0,1,0.1), "Blue")
+showNhsThemeColourTints <- function(tints, colour) {
+  colours <- getNhsColourTints(tints, colour)
+
+  tibble(name = names(colours)) %>%
+    mutate(n = 100 - row_number(),
+           n = sprintf("%03d%s", n, name)) %>%
+    ggplot(aes(n, 1, fill=name, label = name)) +
+    geom_tile() +
+    geom_label(fill = "white") +
+    scale_fill_manual(values = colours) +
+    scale_x_discrete(expand = c(0,0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    coord_flip() +
+    #theme_void() +
+    theme(legend.position = "none",
+          axis.text = element_blank(),
+          axis.ticks = element_blank()) +
+    labs(x = "", y = "")
+}
